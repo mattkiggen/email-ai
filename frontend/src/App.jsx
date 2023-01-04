bfc455a9495f70080cc9154c5971097b3bd2d95f
Matt Kiggen <28730291+mattkiggen@users.noreply.github.com>
2023-01-04 12:18:22 +0200

    Add frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <h1>Use AI to generate email replies</h1>
+      <div>
+        <div>
+          <button>Respectful</button>
+          <button>Casual</button>
+          <button>Excited</button>
+        </div>
+        <textarea cols='50' rows='10'></textarea>
+        <button>Generate reply</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
